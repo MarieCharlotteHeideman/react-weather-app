9b9157d6faf21565942d2f61e64c7e6c148a7841
MarieCharlotteHeideman <mariecharlotteheideman@gmail.com>
2024-05-23 12:54:35 +0200

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -3,26 +3,28 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      Weather App
-      <footer>
-        This project was coded by{" "}
-        <a
-          href="https://subtle-capybara-86bdf3.netlify.app/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Charlotte Heideman
-        </a>{" "}
-        and is open-sourced on{" "}
-        <a
-          href="https://github.com/MarieCharlotteHeideman/react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          GitHub
-        </a>
-        .
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://subtle-capybara-86bdf3.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Charlotte Heideman
+          </a>{" "}
+          and is open-sourced on{" "}
+          <a
+            href="https://github.com/MarieCharlotteHeideman/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            GitHub
+          </a>
+          .
+        </footer>
+      </div>
     </div>
   );
 }
