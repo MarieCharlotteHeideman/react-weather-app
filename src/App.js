c7be46e364da335f9768435399edea6c5398f8b1
MarieCharlotteHeideman <mariecharlotteheideman@gmail.com>
2024-05-23 11:54:10 +0200

    Added links to GitHub repo and portfolio website

--- src/App.js
+++ src/App.js
@@ -1,6 +1,23 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      Weather App
+      <footer>
+        This project was coded by{" "}
+        <a href="https://subtle-capybara-86bdf3.netlify.app/" target="_blank">
+          Charlotte Heideman
+        </a>{" "}
+        and is open-sourced on{" "}
+        <a
+          href="https://github.com/MarieCharlotteHeideman/react-weather-app"
+          target="_blank"
+        >
+          GitHub
+        </a>
+        .
+      </footer>
+    </div>
+  );
 }
