8ad26c87ff4cc65ab92375606d4cd7a155b40fd7
MarieCharlotteHeideman <mariecharlotteheideman@gmail.com>
2024-05-23 12:09:02 +0200

    Improved the links

--- src/App.js
+++ src/App.js
@@ -6,13 +6,18 @@ export default function App() {
       Weather App
       <footer>
         This project was coded by{" "}
-        <a href="https://subtle-capybara-86bdf3.netlify.app/" target="_blank">
+        <a
+          href="https://subtle-capybara-86bdf3.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
           Charlotte Heideman
         </a>{" "}
         and is open-sourced on{" "}
         <a
           href="https://github.com/MarieCharlotteHeideman/react-weather-app"
           target="_blank"
+          rel="noreferrer"
         >
           GitHub
         </a>
